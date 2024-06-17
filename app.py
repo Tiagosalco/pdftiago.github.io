974755e89f4691b509c227498b7b169e4fb3eab4
Tiagosalco <tiagosalco@gmail.com>
2024-06-17 14:32:44 -0300

    V1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
+from flask import Flask, render_template, redirect, url_for
+import subprocess
 
 app = Flask(__name__)
 
@@ -9,22 +9,22 @@ def index():
 
 @app.route('/merge')
 def merge():
-    os.system('python pdfmerger.py')
+    subprocess.call(['python', 'pdfmerger.py'])
     return redirect(url_for('index'))
 
 @app.route('/crop')
 def crop():
-    os.system('python recortarpdf.py')
+    subprocess.call(['python', 'recortarpdf.py'])
     return redirect(url_for('index'))
 
 @app.route('/convert-to-images')
 def convert_to_images():
-    os.system('python PdfA3Fotos.py')
+    subprocess.call(['python', 'pdf_a_tres_fotos.py'])
     return redirect(url_for('index'))
 
 @app.route('/convert-to-pdf')
 def convert_to_pdf():
-    os.system('python imagestopdf.py')
+    subprocess.call(['python', 'imagestopdf.py'])
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
---
+++ pdfmerger.py
@@ -0,0 +1,67 @@
+import tkinter as tk
+from tkinter import filedialog
+import PyPDF2
+import os
+from PIL import Image, ImageTk
+
+class PDFMergerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("PDF Merger")
+        self.root.configure(bg="#f2493d") 
+
+        self.file_list = []
+
+
+        self.frame = tk.Frame(root, padx=10, pady=10, bg="#f2493d")  
+        self.frame.pack()
+
+        self.load_image()
+
+
+        self.add_button = tk.Button(self.frame, text="Add PDFs", command=self.add_pdfs)
+        self.add_button.pack(pady=10, padx=20) 
+
+        self.merge_button = tk.Button(self.frame, text="Merge PDFs", command=self.merge_pdfs)
+        self.merge_button.pack(pady=10, padx=20) 
+
+    def load_image(self):
+
+        image = Image.open("combinar.png")
+        image = image.resize((300, 300))
+        photo = ImageTk.PhotoImage(image)
+
+        self.image_label = tk.Label(self.frame, image=photo, bg="#f2493d")
+        self.image_label.image = photo
+        self.image_label.pack(pady=10)
+
+    def add_pdfs(self):
+        file_paths = filedialog.askopenfilenames(filetypes=[("PDF files", "*.pdf")])
+        if file_paths:
+            self.file_list = list(file_paths)
+
+    def merge_pdfs(self):
+        if not self.file_list:
+            return
+
+        pdf_final = PyPDF2.PdfMerger()
+
+        for file_path in self.file_list:
+            pdf_final.append(file_path)
+
+        output_filename = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")])
+
+        if output_filename:
+            pdf_final.write(output_filename)
+            pdf_final.close()
+            self.file_list = []
+            self.show_merge_success_message(output_filename)
+
+    def show_merge_success_message(self, output_filename):
+        success_label = tk.Label(self.frame, text=f"PDFs merged successfully!\nSaved as {os.path.basename(output_filename)}", bg="#f2493d")  # Set background color
+        success_label.pack(pady=10)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PDFMergerApp(root)
+    root.mainloop()
